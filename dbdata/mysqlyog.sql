40ef59dbeda8e5ad29bc3f0905696ae8888d6bd1
Angela <amuliawan93@gmail.com>
2015-11-30 14:39:02 +0700

    DATABASE mysqlyog.sql

---
+++ dbdata/mysqlyog.sql
@@ -0,0 +1,376 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.6.26 : Database - baz
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`baz` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `baz`;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
+  `CategoryName` varchar(50) DEFAULT NULL,
+  `DegreeID` int(11) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`CategoryID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `category` */
+
+/*Table structure for table `degree` */
+
+DROP TABLE IF EXISTS `degree`;
+
+CREATE TABLE `degree` (
+  `DegreeID` int(11) NOT NULL AUTO_INCREMENT,
+  `DegreeName` varchar(50) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`DegreeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `degree` */
+
+insert  into `degree`(`DegreeID`,`DegreeName`,`AuditedUser`,`AuditedActivity`,`AuditedTime`) values (1,'SMA','Angela','A','2015-11-30 14:24:28');
+
+/*Table structure for table `level` */
+
+DROP TABLE IF EXISTS `level`;
+
+CREATE TABLE `level` (
+  `LevelID` int(11) NOT NULL AUTO_INCREMENT,
+  `LevelName` varchar(50) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`LevelID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `level` */
+
+insert  into `level`(`LevelID`,`LevelName`,`AuditedUser`,`AuditedActivity`,`AuditedTime`) values (1,'Easy','Angela','A','2015-11-30 14:25:48');
+
+/*Table structure for table `test` */
+
+DROP TABLE IF EXISTS `test`;
+
+CREATE TABLE `test` (
+  `TestID` int(11) NOT NULL AUTO_INCREMENT,
+  `TestName` varchar(30) DEFAULT NULL,
+  `TestDescription` varchar(100) DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `LevelID` int(11) DEFAULT NULL,
+  `AccessType` int(11) DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `PublishStartDate` datetime DEFAULT NULL,
+  `PublishEndDate` datetime DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`TestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `test` */
+
+/*Table structure for table `testdetail` */
+
+DROP TABLE IF EXISTS `testdetail`;
+
+CREATE TABLE `testdetail` (
+  `TestDetailID` int(11) NOT NULL AUTO_INCREMENT,
+  `TestQuestion` varchar(200) DEFAULT NULL,
+  `TestURL` varchar(100) DEFAULT NULL,
+  `TestID` int(11) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`TestDetailID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `testdetail` */
+
+/*Table structure for table `testdetailanswer` */
+
+DROP TABLE IF EXISTS `testdetailanswer`;
+
+CREATE TABLE `testdetailanswer` (
+  `TestDetailAnswerID` int(11) NOT NULL AUTO_INCREMENT,
+  `TestDetailAnswerName` varchar(100) DEFAULT NULL,
+  `TestDetailAnswerURL` varchar(100) DEFAULT NULL,
+  `isAnswer` int(11) DEFAULT NULL,
+  `TestDetailID` int(11) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`TestDetailAnswerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `testdetailanswer` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(20) DEFAULT NULL,
+  `FullName` varchar(100) DEFAULT NULL,
+  `UserPhoto` varchar(100) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `UserType` varchar(10) DEFAULT NULL,
+  `Role` varchar(10) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`UserID`,`UserName`,`FullName`,`UserPhoto`,`Password`,`Email`,`UserType`,`Role`,`AuditedUser`,`AuditedActivity`,`AuditedTime`) values (1,'admin','Administrator','admin.jpg','d033e22ae348aeb5660fc2140aec35850c4da997','admin@kertasujian.com','individu','1','Angela','A','2015-11-30 13:37:50'),(2,'alexandrobrian','Brian Alexandro','alexandrobrian.jpg','d033e22ae348aeb5660fc2140aec35850c4da997','alexandrobrian15@gmail.com','individu','0',NULL,'A','2015-11-30 13:39:23'),(3,'Angela','Angela Muliawan','angela.jpg','d033e22ae348aeb5660fc2140aec35850c4da997','amuliawan93@gmail.com','individu','1','Angela','A','2015-11-30 14:31:48');
+
+/*Table structure for table `useranswer` */
+
+DROP TABLE IF EXISTS `useranswer`;
+
+CREATE TABLE `useranswer` (
+  `UserAnswerID` int(11) NOT NULL AUTO_INCREMENT,
+  `TestID` int(11) DEFAULT NULL,
+  `isInvited` int(11) DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `UserEmail` varchar(100) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`UserAnswerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `useranswer` */
+
+/*Table structure for table `useranswerdetail` */
+
+DROP TABLE IF EXISTS `useranswerdetail`;
+
+CREATE TABLE `useranswerdetail` (
+  `UserAnswerDetailID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserAnswerID` int(11) DEFAULT NULL,
+  `TestDetailID` int(11) DEFAULT NULL,
+  `TestDetailAnswerID` int(11) DEFAULT NULL,
+  `AuditedUser` varchar(20) DEFAULT NULL,
+  `AuditedActivity` char(1) DEFAULT NULL,
+  `AuditedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`UserAnswerDetailID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `useranswerdetail` */
+
+/* Procedure structure for procedure `CheckLogin` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `CheckLogin` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckLogin`(
+	In ParamUsername varchar(20),
+	IN ParamPassword varchar(100)
+)
+BEGIN
+	IF EXISTS (SELECT * FROM `User` WHERE UserName = ParamUsername AND Password = ParamPassword AND AuditedActivity <> 'D')
+	THEN
+		SELECT UserID, UserName, UserType, `Role`
+		FROM `User`
+		WHERE 
+		UserName = ParamUsername 
+		AND 
+		Password = ParamPassword 
+		AND 
+		AuditedActivity <> 'D';
+	ELSE
+		SELECT -1 'UserID';
+	END IF;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `DeleteDegree` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `DeleteDegree` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteDegree`(
+	IN ParamDegreeID int,
+	IN ParamAuditedUser varchar(20)
+)
+BEGIN
+	UPDATE Degree
+	SET 
+		AuditedUser = ParamAuditedUser,
+		AuditedActivity = 'D',
+		AuditedTime = CURRENT_TIMESTAMP()
+	WHERE DegreeID = ParamDegreeID;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `DeleteLevel` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `DeleteLevel` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteLevel`(
+	IN ParamLevelID int,
+	IN ParamAuditedUser varchar(20)
+)
+BEGIN
+	UPDATE `Level`
+	SET AuditedUser = ParamAuditedUser,
+	AuditedActivity = 'D',
+	AuditedTime = CURRENT_TIMESTAMP()
+	WHERE LevelID = ParamLevelID;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `EditDegree` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `EditDegree` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `EditDegree`(
+	IN ParamDegreeID int,
+	IN ParamDegreeName varchar(50),
+	IN ParamAuditedUser varchar(20)
+)
+BEGIN
+	UPDATE Degree
+	SET 
+		DegreeName = ParamDegreeName,
+		AuditedUser = ParamAuditedUser,
+		AuditedActivity = 'E',
+		AuditedTime = CURRENT_TIMESTAMP()
+	WHERE DegreeID = ParamDegreeID;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `EditLevel` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `EditLevel` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `EditLevel`(
+	IN ParamLevelID int,
+	in ParamLevelName varchar(50),
+	IN ParamAuditedUser varchar(20)
+)
+BEGIN
+	UPDATE `Level`
+	SET LevelName = ParamLevelName,
+	AuditedUser = ParamAuditedUser,
+	AuditedActivity = 'E',
+	AuditedTime = CURRENT_TIMESTAMP()
+	WHERE LevelID = ParamLevelID;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `GetDegree` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `GetDegree` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetDegree`()
+BEGIN
+	SELECT DegreeID, DegreeName FROM `Degree`
+	WHERE AuditedActivity <> 'D';
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `GetLevel` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `GetLevel` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetLevel`()
+BEGIN
+	SELECT LevelID, LevelName FROM `Level`
+	WHERE AuditedActivity <> 'D';
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `InsertDegree` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `InsertDegree` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertDegree`(
+	IN ParamDegreeName varchar(50),
+	IN ParamAuditedUser varchar(20)
+)
+BEGIN
+	INSERT INTO Degree (DegreeName, AuditedUser, AuditedActivity, AuditedTime) VALUES (ParamDegreeName, ParamAuditedUser, 'A', CURRENT_TIMESTAMP());
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `InsertLevel` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `InsertLevel` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertLevel`(
+	IN ParamLevelName varchar(50),
+	IN ParamAuditedUser varchar(20)
+)
+BEGIN
+	INSERT INTO Level (levelName, AuditedUser, AuditedActivity, AuditedTime) VALUES (ParamLevelName, ParamAuditedUser, 'A', CURRENT_TIMESTAMP());
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `InsertUser` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `InsertUser` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertUser`(
+	ParamUserName varchar(20),
+	ParamFullName varchar(100),
+	ParamUserPhoto varchar(100),
+	ParamPassword varchar(100),
+	ParamEmail varchar(100),
+	ParamUserType varchar(10),
+	ParamAuditedUser varchar(20)
+)
+BEGIN
+	
+	INSERT INTO `User` 
+	(UserName, FullName, UserPhoto, `Password`, Email, UserType, Role, AuditedUser, AuditedActivity, AuditedTime)
+	VALUES 
+	(ParamUserName, ParamFullName, ParamUserPhoto, ParamPassword, ParamEmail, ParamUserType, 0, ParamAuditedUser, 'A', CURRENT_TIMESTAMP());
+    END */$$
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
