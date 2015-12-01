ac53ac111d3745891abdf2fef7d92c810ee97a9c
brianalexandro <alexandrobrian15@gmail.com>
2015-12-01 20:21:35 +0700

    db sample

--- application/config/database - sample.php
+++ application/config/database - sample.php
@@ -48,14 +48,16 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-//Dummy
-//$db['default']['hostname'] = 'HP-PC\SQLEXPRESS'; 
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+
 $db['default']['database'] = 'baz';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = FALSE;
-$db['default']['db_debug'] = FALSE;
-$db['default']['cache_on'] = FALSE;
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = TRUE;
 $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
